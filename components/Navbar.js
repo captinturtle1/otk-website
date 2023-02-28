a27f2ea41949e9f6df1e34596645f42c2e024880
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2023-02-28 11:02:00 -0800

    navbar

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import logo from '../public/whiteLogo.png';
+
+export default function Navbar() {
+    return (
+    <div className='bg-black h-16 select-none'>
+      <div className='mx-32 flex text-lg font-semibold'>
+        <div className='m-auto cursor-pointer text-white hover:text-gray-200 transition-all'>Creators</div>
+        <div className='m-auto mr-32 cursor-pointer text-white hover:text-gray-200 transition-all'>Events</div>
+        <Image src={logo} className='w-16 h-16 p-2'/>
+        <div className='m-auto ml-32 cursor-pointer text-white hover:text-gray-200 transition-all'>Creators</div>
+        <div className='m-auto cursor-pointer text-white hover:text-gray-200 transition-all'>Shop</div>
+      </div>
+    </div>
+  )
+}  
