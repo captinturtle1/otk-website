3b7ec94f48e9736094fcaf4ce271a52241e838d6
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2023-03-02 13:59:30 -0800

    Update Navbar.js
    changed section names

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,10 +8,10 @@ export default function Navbar({sidebarVisible, setSidebarVisible}) {
     <div className='bg-black h-16 w-screen select-none absolute z-50'>
     <FaStepForward onClick={sidebarVisible ? () => setSidebarVisible(false) : () => setSidebarVisible(true)} className={sidebarVisible ? 'text-white fixed top-6 left-5 rotate-180 cursor-pointer transition-all' : 'text-white fixed top-6 left-5 cursor-pointer transition-all'}/>
       <div className='mx-32 flex text-lg font-semibold'>
-        <div className='m-auto cursor-pointer text-white hover:text-gray-200 transition-all'>Creators</div>
+        <div className='m-auto cursor-pointer text-white hover:text-gray-200 transition-all'>Live</div>
         <div className='m-auto cursor-pointer text-white hover:text-gray-200 transition-all'>Events</div>
         <Image src={logo} className='w-16 h-16 p-2'/>
-        <div className='m-auto cursor-pointer text-white hover:text-gray-200 transition-all'>Creators</div>
+        <div className='m-auto cursor-pointer text-white hover:text-gray-200 transition-all'>Videos</div>
         <div className='m-auto cursor-pointer text-white hover:text-gray-200 transition-all'>Shop</div>
       </div>
     </div>
