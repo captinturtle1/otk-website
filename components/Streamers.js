999af64f13ee95b801eb02833a1ccbe969616cb8
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2023-03-08 04:28:07 -0800

    Update Streamers.js
    added hover shadow

--- components/Streamers.js
+++ components/Streamers.js
@@ -9,7 +9,9 @@ export default function Events() {
     <div className='w-full py-12 flex' id='Streamers'>
         <div className='w-full h-full p-20 grid grid-cols-5 gap-x-8 gap-y-12'>
             {streamerInfo.map((streamer, index) => 
-                <div className='flex flex-col bg-yellow-400 text-white font-bold text-3xl rounded-xl p-5 drop-shadow-xl'>
+            <div className='flex flex-col relative'>
+                <div className='bg-black w-full h-full -z-10 absolute rounded-2xl'/>
+                <div className='flex flex-col bg-yellow-400 text-white font-bold text-3xl rounded-xl p-5 drop-shadow-xl hover:-translate-x-3 hover:-translate-y-3 transition-all'>
                     <Image src={streamer.image2} className='rounded-xl drop-shadow-xl'/>
                     <div className='m-auto mt-5'>{streamer.name}</div>
                     <div className='flex m-auto mt-5 gap-10'>
@@ -18,6 +20,8 @@ export default function Events() {
                         <a href={streamer.twitter} className=''><FaTwitter/> </a>
                     </div>
                 </div>
+                
+            </div>
             )}
         </div>
     </div>
