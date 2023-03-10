a3d49041062bd039eb38522c478208671e382d43
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2023-03-09 21:24:22 -0800

    Update Sidebar.js
    support for smaller screens

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -58,17 +58,17 @@ export default function Sidebar({streamerObjects, sidebarVisible}) {
     );
 
     return (
-    <div className={sidebarVisible ? ' w-64 flex-shrink-0 z-40 transition-all' : 'w-0 flex-shrink-0 z-40 transition-all'}>
-        <div className={sidebarVisible ? 'bg-zinc-200 h-screen flex flex-col pt-24 w-64 fixed left-0 transition-all p-6': 'bg-zinc-200 h-screen flex flex-col pt-24 w-64 fixed left-[-15%] transition-all p-6'}>
-            <div className='overflow-x-scroll flex flex-col gap-2'>
-                {streamerList}
+        <div className={sidebarVisible ? ' w-64 flex-shrink-0 z-40 transition-all hidden md:block' : 'w-0 flex-shrink-0 z-40 transition-all hidden md:block'}>
+            <div className={sidebarVisible ? 'bg-zinc-200 h-screen flex flex-col pt-24 w-64 fixed left-0 transition-all p-6': 'bg-zinc-200 h-screen flex flex-col pt-24 w-64 fixed left-[-100%] transition-all p-6'}>
+                <div className='overflow-x-scroll flex flex-col gap-2'>
+                    {streamerList}
+                </div>
+                {!expanded ? (
+                    <div className='mx-auto font-bold cursor-pointer' onClick={() => setExpanded(true)}>show more</div>
+                ):(
+                    <div className='mx-auto font-bold cursor-pointer mt-8' onClick={() => setExpanded(false)}>show less</div>
+                )}
             </div>
-            {!expanded ? (
-                <div className='mx-auto font-bold cursor-pointer' onClick={() => setExpanded(true)}>show more</div>
-            ):(
-                <div className='mx-auto font-bold cursor-pointer mt-8' onClick={() => setExpanded(false)}>show less</div>
-            )}
         </div>
-    </div>
     )
 } 
