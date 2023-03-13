c1ce44b4f7b84dddc3ce35268d4801662996aaba
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2023-03-13 13:48:02 -0700

    Update Footer.js
    added not official warning and added credit with link to twitter

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ import { FaTwitter, FaTwitch, FaYoutube, FaInstagram, FaTiktok, FaDiscord, FaRed
 
 export default function Footer() {
     return (
-    <div className='flex bg-black w-full h-64' id='Footer'>
+    <div className='flex bg-black w-full p-12' id='Footer'>
         <div className='m-auto flex flex-col gap-10'>
             <div className='m-auto'>
                 <Image src={whiteLogo} className=''/>
@@ -20,6 +20,10 @@ export default function Footer() {
                 <a href='https://discord.com/invite/m8bgxM8'><FaDiscord/></a>
                 <a href='https://www.reddit.com/r/OTKNetwork'><FaRedditAlien/></a>
                 <a href='mailto:partners@otknetwork.com'><FaRegEnvelope/></a>
+            </div>
+            <div className='m-auto text-sm text-center'>
+                <div className='text-white mx-auto'>Not an official site</div>
+                <a href='https://twitter.com/captinturt1e/' className='text-white mx-auto hover:text-blue-400 transition-all'>Made by captinturtle</a>
             </div>
         </div>
     </div>
