a3b04c16160af6c2ccc708ee7123a362c2446bb6
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2023-07-31 15:05:03 -0600

    Update getTwitchData.js
    Automated getting a new twitch api access token using twitch client secret if oauth is invalid

--- pages/api/getTwitchData.js
+++ pages/api/getTwitchData.js
@@ -1,3 +1,20 @@
+let accessToken = '';
+
+fetch('https://id.twitch.tv/oauth2/token', {
+    method: 'POST',
+    headers:{
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },    
+    body: new URLSearchParams({
+        'client_id': process.env.TWITCH_CLIENT_ID,
+        'client_secret': process.env.TWITCH_CLIENT_SECRET,
+        'grant_type': 'client_credentials'
+    })
+})
+.then((response) => response.json())
+.then((data) => accessToken = data.access_token)
+.catch(console.log);
+
 export default function handler(req, res) {
     return new Promise((resolve, reject) => {
         if (req.method === 'POST') {
@@ -13,18 +30,53 @@ export default function handler(req, res) {
                 fetch(`https://api.twitch.tv/helix/streams?${urlParems}`, {
                     method: "GET",
                     headers: {
-                        'Authorization': 'Bearer ' + process.env.access_token,
-                        'Client-Id': process.env.client_id
+                        'Authorization': 'Bearer ' + accessToken,
+                        'Client-Id': process.env.TWITCH_CLIENT_ID
                     }
                 })
                 .then((response) => response.json())
                 .then((data) => {
-                    res.status(200).json({ data });
-                    resolve();
+                    if (data.error) {
+                        // if token is invalid, get a new token
+                        fetch('https://id.twitch.tv/oauth2/token', {
+                            method: 'POST',
+                            headers:{
+                                'Content-Type': 'application/x-www-form-urlencoded'
+                            },    
+                            body: new URLSearchParams({
+                                'client_id': process.env.TWITCH_CLIENT_ID,
+                                'client_secret': process.env.TWITCH_CLIENT_SECRET,
+                                'grant_type': 'client_credentials'
+                            })
+                        })
+                        .then((response) => response.json())
+                        .then((data) => {
+                            // setting new token
+                            accessToken = data.access_token;
+                            // trying to get streams again with new token set
+                            fetch(`https://api.twitch.tv/helix/streams?${urlParems}`, {
+                                method: "GET",
+                                headers: {
+                                    'Authorization': 'Bearer ' + accessToken,
+                                    'Client-Id': process.env.TWITCH_CLIENT_ID
+                                }
+                            })
+                            .then((response) => response.json())
+                            .then((data) => {
+                                res.status(200).json({ data });
+                                resolve();
+                            })
+                        })
+                    } else {
+                        // if token is good
+                        res.status(200).json({ data });
+                        resolve();
+                    }
                 })
             } catch(err) {
                 res.status(500).json({ error: '500 Internal Server Error'});
                 reject();
+                console.log(err);
             }
         } else {
             res.status(400).json({ error: '400 Bad Request'});
