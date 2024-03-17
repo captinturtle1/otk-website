f54a595afc15f1e98fa27bb6c859cf5e09cf57a9
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2024-03-17 16:13:23 -0600

    make highlight darker & reduced padding

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -16,7 +16,7 @@ export default function Sidebar({streamerObjects, sidebarVisible}) {
                 <>
                     {index < 4 ? (
                         <div>
-                            <a href={`https://twitch.tv/${streamer.name}`} target='_blank' className="flex gap-2 w-40 bg-zinc-400 bg-opacity-0 hover:bg-opacity-20 p-1 rounded transition-all">
+                            <a href={`https://twitch.tv/${streamer.name}`} target='_blank' className="flex gap-2 w-full bg-zinc-400 bg-opacity-0 hover:bg-opacity-20 p-1 rounded transition-all">
                                 <Image height={50} width={50} src={streamer.pfp} className='rounded-full'/>
                                 <div className='my-auto flex flex-col'>
                                     <div className='font-semibold text-lg'>{streamer.name}</div>
@@ -58,7 +58,7 @@ export default function Sidebar({streamerObjects, sidebarVisible}) {
 
     return (
         <div className={sidebarVisible ? 'w-64 flex-shrink-0 z-40 transition-all absolute md:static' : 'w-0 flex-shrink-0 z-40 transition-all absolute md:static'}>
-            <div className={sidebarVisible ? 'bg-zinc-200 h-screen flex flex-col pt-24 w-64 fixed left-0 transition-all p-6': 'bg-zinc-200 h-screen flex flex-col pt-24 w-64 fixed -left-64 transition-all p-6'}>
+            <div className={sidebarVisible ? 'bg-zinc-300 h-screen flex flex-col pt-24 w-64 fixed left-0 transition-all p-4': 'bg-zinc-200 h-screen flex flex-col pt-24 w-64 fixed -left-64 transition-all p-6'}>
                 <div className='flex flex-col gap-2'>
                     {streamerList}
                 </div>
